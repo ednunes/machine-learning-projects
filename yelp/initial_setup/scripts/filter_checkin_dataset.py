1a55cee5f5361495e5f8f71f072ea3e2906065e9
Mateus Manuel <mateusmanuel21@hotmail.com>
2018-12-04 11:12:16 -0200

    Upload datasets and improve checkin filter

--- yelp/initial_setup/scripts/filter_checkin_dataset.py
+++ yelp/initial_setup/scripts/filter_checkin_dataset.py
@@ -4,17 +4,37 @@ from pprint import pprint
 with open('../dataset/yelp_academic_dataset_checkin.json') as f:
     data = json.load(f)
 
+with open('business_id') as f:
+    business_id = f.read().splitlines()
 
 with open('../dataset/yelp_academic_dataset_checkin.csv', 'w') as f:
     f.write('business_id,checkin_count\n')
 
-    for i in range(0, len(data)):
-        i_sum = 0
-        
-        for attr, value in data[i]['time'].items():
-            i_sum += value
+    j = 0
 
-        f.write('{0},{1}\n'.format(data[i]['business_id'], i_sum))
+    for i in range(0, len(business_id)):
+        j_sum = 0
+        
+        if data[j]['business_id'] != business_id[i]:
+            f.write('{0},{1}\n'.format(business_id[i], 0))
+            continue
+
+        for attr, value in data[j]['time'].items():
+            j_sum += value
+
+        f.write('{0},{1}\n'.format(data[j]['business_id'], j_sum))
+        j += 1
+
+# with open('../dataset/yelp_academic_dataset_checkin.csv', 'w') as f1:
+#     f.write('business_id,checkin_count\n')
+
+#     for i in range(0, len(data)):
+#         i_sum = 0
+        
+#         for attr, value in data[i]['time'].items():
+#             i_sum += value
+
+#         f.write('{0},{1}\n'.format(data[i]['business_id'], i_sum))
 
 
 
